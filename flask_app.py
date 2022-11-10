24a9bac5eb197a56b5cbb3024f60e9dd168534b6
div2der <96227881+div2der@users.noreply.github.com>
2022-11-10 08:02:54 +0600

    posts added

--- flask_app.py
+++ flask_app.py
@@ -4,7 +4,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = r'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
--- main.py
+++ main.py
@@ -2,16 +2,47 @@ from flask_app import *
 from flask import render_template
 from flask import request, session, redirect, url_for
 from werkzeug.utils import secure_filename
+import hashlib
+import os
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 
+class User(db.Model):
+    __tablename__ = "User"
+    user_id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.VARCHAR(255), unique = True, nullable = False)
+    email = db.Column(db.VARCHAR(255))
+    password = db.Column(db.VARCHAR(255), nullable = False)
+    user_posts = db.relationship("Posts", back_populates = "owner", cascade = "all, delete-orphan")
 
+class Posts(db.Model):
+    __tablename__ = "Posts"
+    post_id = db.Column(db.Integer, primary_key = True)
+    post_username = db.Column(db.VARCHAR(255), db.ForeignKey("User.username"), nullable = False)
+    post_text = db.Column(db.VARCHAR(255))
+    post_media_name = db.Column(db.VARCHAR(255))
+    owner = db.relationship("User", back_populates = "user_posts")
+
+def addUser(user:User) -> None:
+    db.session.add(user)
+    db.session.commit()
+
+def addPost(post:Posts) -> None:
+    db.session.add(post)
+    db.session.commit()
+
+def hash_password(password):
+    return hashlib.md5(password.encode('utf-8')).hexdigest()
 
 @app.route("/home")
-@app.route("/")
-def home(context=None):
-    data = {"Data":"Some data here to be sent as dict (JSON)"}
-    return render_template("base.html", context=None)
+def home(context = None):
+    posts = Posts.query.all()
+    return render_template("base.html", posts = posts[::-1])
 
+@app.route("/")
+def index(context=None):
+    return render_template("base.html", context=None)
 
 @app.route("/user/<int:user_id>")
 def user_page(user_id, context=None):
@@ -22,19 +53,33 @@ def user_page(user_id, context=None):
         query = db.session.query(User).filter(User.user_id == user_id).first()
         return render_template("user page.html", context=query)
 
+@app.route('/register', methods = ["GET", "POST"])
+def register(context=None):
+    if request.method == "POST":
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        user = db.session.query(User).filter_by(username = username).first()
+        if user:
+            return redirect(url_for("register", context = "Already registered!"))
+        elif password != confirm_password:
+            return redirect(url_for("register", context = "Passwords don't match!"))
+        addUser(User(username = username, email = email, password = hash_password(password)))
+        return redirect(url_for("login", context = "Succesfully registered"))
+    return render_template("register.html", context = None)
+
 @app.route("/login", methods = ["GET", "POST"])
 def login(context=None):
     if request.method == "POST":
-        user = db.session.query(User).filter_by(login=request.form['username'], password=request.form['password']).first()
-        print(user)
+        user = db.session.query(User).filter_by(username = request.form['username'], password = hash_password(request.form['password'])).first()
         if user:
             session['authenticated'] = True
             session['uid'] = user.user_id
-            session['username'] = user.login
-            return redirect(url_for("user_page", user_id=user.user_id))
+            session['username'] = user.username
+            return redirect(url_for("home"))
         else:
-            return render_template("login.html", context="The login or username were wrong")
-
+            return render_template("login.html", context="Username or password is wrong. Try again.")
     return render_template("login.html", context=context)
 
 @app.route("/logout")
@@ -44,43 +89,21 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('home'))
 
-@app.route('/register', methods = ["GET", "POST"])
-def register(context=None):
+@app.route("/post_upload", methods = ["GET", "POST"])
+def post_upload(context = None):
     if request.method == "POST":
-        login = request.form['username']
-        fname = request.form['fname']
-        sname = request.form['sname']
-        pass1 = request.form['password']
-        pass2 = request.form['password_conf']
-        email = request.form['email']
-        mobile = request.form['mobile']
-        d_of_b = request.form['d_of_b']
-        loc = request.form['loc']
-        info = request.form['info']
-        data = db.session.query(User).filter_by(login=request.form['username']).first()
-        
-        print(data)
-
-        if data :
-            return redirect(url_for("register", error="Already registered!"))
-        elif pass1!=pass2:
-            return redirect(url_for("register", error="Passowords do not match!"))
-        else:
-            crud.add_user(User(login=login, 
-                                user_fname=fname,
-                                user_sname=sname,
-                                password=pass1, 
-                                email=email,
-                                mobile=mobile,
-                                d_of_b=d_of_b,
-                                loc=loc,
-                                info=info
-                                ))
-
-            print(pass2, login)
-
-            return redirect(url_for("login", context="Succesfully registered!"))
-    return render_template("registration.html", context=context)
+        caption = request.form["caption"]
+        image = request.files["image_to_upload"]
+        try:
+            image.save(f"static/files/{session['username']}/{secure_filename(image.filename)}")
+        except FileNotFoundError:
+            os.mkdir(f"static/files/{session['username']}")
+            image.save(f"static/files/{session['username']}/{secure_filename(image.filename)}")
+        post = Posts(post_username = session['username'], post_text = caption, post_media_name = secure_filename(image.filename))
+        db.session.add(post)
+        db.session.commit()
+        return render_template("post_upload.html", context = "Post succesfully uploaded!")
+    return render_template("post_upload.html", context = None) 
 
 @app.route("/upload", methods=["GET", "POST"])
 def upload_file(context=None):
@@ -108,15 +131,12 @@ def add(context=None):
                                 text_about=info_pr,
                                 pr_author=session['uid']
                                 ))
-
-            
-
             return redirect(url_for("profile", context="Succesfully registered!"))
     return render_template("add_pr.html", context=context)
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-        app.run(port=2000)
+        app.run()
 
 
